e8fbae1ac42b41165c47bb32368be72406332cd9
Leckller <7leckler@gmail.com>
2024-04-11 16:33:04 -0300

    feat: envs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const serverPort = process.env.SERVER_PORT || 7771;
+const serverPort = process.env.PORT || 7771;
 
 app.listen(serverPort, () => {
   console.log(`Server ON na porta ${serverPort}`)
